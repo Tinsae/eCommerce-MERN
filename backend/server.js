82bfb9cf012652b52743893ffa98216f5b8b8d3b
Tinsae <tigial3535@gmail.com>
2021-06-05 01:24:18 -0500

    App Deployment

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,6 @@ app.use(express.json())
 
 connectDB();
 
-app.get("/", (req, res) => {
-    res.send("API is running")
-})
-
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use('/api/orders', orderRoutes)
@@ -39,6 +35,22 @@ app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....')
+    })
+}
+
+
+
 
 
 // error handling
